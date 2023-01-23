30c9cbb8e21e54c26de1b060b6d0f84aa93c7162
Nathalon <braindisassemblue@gmail.com>
2023-01-23 08:44:05 +0200

    Update shellean.sh

--- shellean.sh
+++ shellean.sh
@@ -62,16 +62,16 @@ if [ -z $ip ];
   then
     echo '[!] -> You need to specify a Internet Protocol Address';
     echo '';
-
     help;
+    
     exit;
 
 elif [ -z $port ];
   then
     echo '[!] -> You need to specify a Port';
     echo '';
-
     help;
+    
     exit;
 
 else
